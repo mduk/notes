ef3958c3c3eb5d55cb56eec7b180384506b97b57
Daniel Kendell <daniel@kendell.org>
2014-12-15 23:14:52 +0000

    Moved the magic madness into a magic trait. I feel dirty.

--- src/Mapper/Pdo.php
+++ src/Mapper/Pdo.php
@@ -4,6 +4,7 @@ namespace Mduk\Mapper;
 
 use Mduk\Mapper;
 use Mduk\Mapper\Factory as Factory;
+use Mduk\MagicQueryBuilder;
 
 use Mduk\Collection;
 use Mduk\LazyCollection;
@@ -29,6 +30,8 @@ use Mduk\Identity\Map\Memory as IdentityMapMemory;
  *   *Into           : Insert objects into a specific collection instance
  */
 abstract class Pdo implements Mapper {
+	use MagicQueryBuilder;
+
 	protected $db;
 	protected $identityMap;
 	protected $mapperFactory;
@@ -73,143 +76,6 @@ abstract class Pdo implements Mapper {
 	 */
 	public function executeFind( Query $query ) {
 		return $this->executeQuery( 'find', $query );
-	}
-
-	/**
-	 * Build a Query object based on the name of the method and the arguments
-	 * passed in, execute the Query, and return the result.
-	 */
-	public function __call( $method, $arguments ) {
-		$query = new Query(
-			$this,
-			$this->findSelect,
-			$this->loadSelect,
-			$this->countSelect,
-			$this->table
-		);
-		
-		// What will be done with the query
-		if ( strpos( $method, 'load' ) === 0 ) {
-			$exec = 'load';
-			$method = substr( $method, 4 );
-		}
-		else if ( strpos( $method, 'find' ) === 0 ) {
-			$exec = 'find';
-			$method = substr( $method, 4 );
-		}
-		else if ( strpos( $method, 'count' ) === 0 ) {
-			$exec = 'count';
-			$method = substr( $method, 5 );
-		}
-		else if ( strpos( $method, 'query' ) === 0 ) {
-			$exec = 'query';
-			$method = substr( $method, 5 );
-		}
-		else if ( strpos( $method, 'lazy' ) === 0 ) {
-			$exec = 'lazy';
-			$method = substr( $method, 4 );
-		}
-		else {
-			throw new \Exception( "Unknown method: $method" );
-		}
-		
-		// Limit the number of records retrieved
-		if ( strpos( $method, 'One' ) === 0 ) {
-			$query->limit( 1 );
-			$method = substr( $method, 3 );
-		}
-		else if ( strpos( $method, 'Range' ) === 0 ) {
-			$query->offset( array_shift( $arguments ) );
-			$query->limit( array_shift( $arguments ) );
-			$method = substr( $method, 5 );
-		}
-		
-		// Find particular records by matching fields to arguments
-		if ( strpos( $method, 'By' ) === 0 ) {
-			$method = substr( $method, 2 );
-			$fields = explode( 'And', $method );
-			$where = array();
-			for ( $i = 0; $i < count( $fields ); $i++ ) {
-				$field = $this->camel2snake( $fields[ $i ] );
-				$where[] .= $field . ' = :' . $field;
-				$value = array_shift( $arguments );
-
-				if ( $value === null ) {
-					throw new Exception( 'Missing field value: ' . $field );
-				}
-
-				$query->bindValue( ':' . $field, $value );
-			}
-			$where = implode( ' AND ', $where );
-			$query->where( $where );
-		}
-		
-		// When inserting the object into the collection, key them by a particular field
-		if ( strpos( $method, 'KeyedBy' ) === 0 ) {
-			$method = substr( $method, 7 );
-			$field = '';
-			$intoPos = strpos( $method, 'Into' );
-
-			if ( $intoPos === 0 || strlen( $method ) == 0 ) {
-				throw new Exception( "Missing field to key by!" );
-			}
-
-			$keyField = $this->camel2snake( $method );
-			$query->keyBy( $keyField );
-		}
-
-		// Insert objects into a specific collection instance
-		if ( strpos( $method, 'Into' ) === 0 ) {
-			$collection = array_shift( $arguments );
-
-			if ( $collection === null ) {
-				throw new Exception( 'Missing collection to load into' );
-			}
-
-			$query->collection( $collection );
-		}
-
-		// Exec: Query - Just return the query object
-		if ( $exec == 'query' ) {
-			return $query;
-		}
-
-		// Exec: Lazy - Return a lazy collection containing this query
-		if ( $exec == 'lazy' ) {
-			return new LazyCollection( $query );
-		}
-
-		// Exec: Count - Return the count
-		if ( $exec == 'count' ) {
-			return $query->count();
-		}
-
-		// Exec: Load/Find
-		if ( $exec == 'load' ) {
-			$result = $query->load();
-		}
-		else {
-			$result = $query->find();
-		}
-
-		// Looking for one record
-		if ( $query->limit() == 1 ) {
-			$count = $result->count();
-
-			if ( $count == 0 ) {
-				throw new \Exception( 'Object not found! SQL: ' . $query->toSql( $exec, true ) );
-			}
-
-			if ( $count > 1 ) {
-				throw new \Exception( 'Multiple objects found! SQL: ' . $query->toSql( $exec, true ) );
-			}
-
-			if ( $exec == 'load' ) {
-				$result = $result->shift();
-			}
-		}
-
-		return $result;
 	}
 
 	/**
