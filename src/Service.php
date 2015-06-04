c5cfdf627952cfc24ee21f941006b13546ccf5a9
Daniel Kendell <daniel@kendell.org>
2015-06-05 00:16:05 +0100

    forgot that bit when renaming query to request

--- src/Service.php
+++ src/Service.php
@@ -6,7 +6,7 @@ use Mduk\Service\Request as ServiceRequest;
 use Mduk\Service\Response as ServiceResponse;
 
 interface Service {
-  public function query( $call );
+  public function request( $call );
   public function execute( ServiceRequest $q, ServiceResponse $r);
 }
 
