87ea949a6bf4a649f3ff1a15f59d5d0930cdfe02
Daniel Kendell <daniel@kendell.org>
2015-06-05 00:58:24 +0100

    tabs to spaces

--- src/Rest/Endpoint.php
+++ src/Rest/Endpoint.php
@@ -20,32 +20,32 @@ use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class Endpoint {
 
-	protected $routes;
+  protected $routes;
   protected $serviceFactory;
-	protected $transcoderFactory;
+  protected $transcoderFactory;
 
-	public function __construct(
+  public function __construct(
     array $routes,
     ServiceFactory $serviceFactory,
     TranscoderFactory $transcoderFactory
   ) {
-		$this->routes = $this->initialiseRoutes( $routes );
+    $this->routes = $this->initialiseRoutes( $routes );
     $this->serviceFactory = $serviceFactory;
-		$this->transcoderFactory = $transcoderFactory;
-	}
+    $this->transcoderFactory = $transcoderFactory;
+  }
 
-	public function handle( Request $request ) {
-		try {
-			// Which Route matches this request?
-			$route = $this->matchRoute( $request );
+  public function handle( Request $request ) {
+    try {
+      // Which Route matches this request?
+      $route = $this->matchRoute( $request );
 
-			// Deny all methods that weren't declared
-			if ( !isset( $route[ $request->getMethod() ] ) ) {
-				throw new EndpointException(
-					"Unsupported Method",
-					EndpointException::UNSUPPORTED_METHOD
-				);
-			}
+      // Deny all methods that weren't declared
+      if ( !isset( $route[ $request->getMethod() ] ) ) {
+        throw new EndpointException(
+          "Unsupported Method",
+          EndpointException::UNSUPPORTED_METHOD
+        );
+      }
 
       $service = $this->serviceFactory->get( $route['service'] );
       $routeMethod = $route[ $request->getMethod() ];
@@ -53,8 +53,8 @@ class Endpoint {
       // How should we encode the response?
       $transcoder = $this->resolveTranscoder( $request, $routeMethod );
 
-			switch ( $request->getMethod() ) {
-				case Request::METHOD_GET:
+      switch ( $request->getMethod() ) {
+        case Request::METHOD_GET:
           $serviceRequest = $service->request( $routeMethod['call'] );
  
           foreach ( $route['bind'] as $bind ) {
@@ -63,9 +63,9 @@ class Endpoint {
 
           $collection = $serviceRequest->execute()->getResults();
 
-					// What to encode? Just one, or a page.
-					if ( isset( $routeMethod['multiplicity'] ) && $routeMethod['multiplicity'] == 'one' ) {
-						$encode = $collection->shift();
+          // What to encode? Just one, or a page.
+          if ( isset( $routeMethod['multiplicity'] ) && $routeMethod['multiplicity'] == 'one' ) {
+            $encode = $collection->shift();
 
             // If the multiplicity is one, then we expect one.
             if ( !$encode ) {
@@ -73,104 +73,104 @@ class Endpoint {
               $response->setStatusCode( 404 );
               return $response;
             }
-					}
-					else {
-						$page = $request->query->get( 'page', 1 );
-						$encode = $collection->page( $page - 1 );
-					}
+          }
+          else {
+            $page = $request->query->get( 'page', 1 );
+            $encode = $collection->page( $page - 1 );
+          }
 
-					// Encode them.
-					$encoded = $transcoder->encode( $encode );
+          // Encode them.
+          $encoded = $transcoder->encode( $encode );
 
-					// Respond
-					$response = new Response();
-					$response->setStatusCode( 200 );
-					$response->setContent( $encoded );
-					return $response;
+          // Respond
+          $response = new Response();
+          $response->setStatusCode( 200 );
+          $response->setContent( $encoded );
+          return $response;
 
-				default:
-					throw new EndpointException(
-						"Unsupported method",
-						EndpointException::UNSUPPORTED_METHOD
-					);
-			}
-		}
-		catch ( ResourceNotFoundException $e ) {
-			$response = new Response();
-			$response->setStatusCode( 404 );
-			return $response;
-		}
+        default:
+          throw new EndpointException(
+            "Unsupported method",
+            EndpointException::UNSUPPORTED_METHOD
+          );
+      }
+    }
+    catch ( ResourceNotFoundException $e ) {
+      $response = new Response();
+      $response->setStatusCode( 404 );
+      return $response;
+    }
     /*
       This might have something to do with making the 404 over 501 test pass again...
-		catch ( MapperException $e ) {
-			switch ( $e->getCode() ) {
-				case MapperException::UNEXPECTED_ROW_COUNT:
-					$response = new Response();
-					$status = ( $e->rowCount == 0 ) ? 404 : 500;
-					$response->setStatusCode( $status );
-					return $response;
-			}
-		}
+    catch ( MapperException $e ) {
+      switch ( $e->getCode() ) {
+        case MapperException::UNEXPECTED_ROW_COUNT:
+          $response = new Response();
+          $status = ( $e->rowCount == 0 ) ? 404 : 500;
+          $response->setStatusCode( $status );
+          return $response;
+      }
+    }
     */
-		catch ( EndpointException $e ) {
-			switch ( $e->getCode() ) {
-				case EndpointException::CAN_NOT_FULFIL_ACCEPT_HEADER:
-					$response = new Response();
-					$response->setStatusCode( 406 );
-					return $response;
+    catch ( EndpointException $e ) {
+      switch ( $e->getCode() ) {
+        case EndpointException::CAN_NOT_FULFIL_ACCEPT_HEADER:
+          $response = new Response();
+          $response->setStatusCode( 406 );
+          return $response;
 
-				case EndpointException::UNSUPPORTED_METHOD:
-					$response = new Response();
-					$response->setStatusCode( 501 );
-					return $response;
-				
-				default:
-					throw $e;
-			}
-		}
-	}
+        case EndpointException::UNSUPPORTED_METHOD:
+          $response = new Response();
+          $response->setStatusCode( 501 );
+          return $response;
+        
+        default:
+          throw $e;
+      }
+    }
+  }
 
-	protected function resolveTranscoder( $request, $route ) {
-		$providedContentTypes = isset( $route['content_types'] ) ? $route['content_types'] : array();
-		$acceptedContentTypes = $request->getAcceptableContentTypes();
-		foreach ( $acceptedContentTypes as $mime ) {
+  protected function resolveTranscoder( $request, $route ) {
+    $providedContentTypes = isset( $route['content_types'] ) ? $route['content_types'] : array();
+    $acceptedContentTypes = $request->getAcceptableContentTypes();
+    foreach ( $acceptedContentTypes as $mime ) {
       if ( !isset( $providedContentTypes[ $mime ] ) ) {
-				continue;
-			}
+        continue;
+      }
 
-			try {
-				return $this->transcoderFactory->getTranscoder( $providedContentTypes[ $mime ] );
-			}
-			catch ( \Exception $e ) {
+      try {
+        return $this->transcoderFactory->getTranscoder( $providedContentTypes[ $mime ] );
+      }
+      catch ( \Exception $e ) {
 
-			}
-		}
-		
-		throw new EndpointException(
-			"No transcoder found for any acceptable mime types",
-			EndpointException::CAN_NOT_FULFIL_ACCEPT_HEADER
-		);
-	}
+      }
+    }
 
-	protected function initialiseRoutes( $routeConfig ) {
-		$routes = new RouteCollection();
-		foreach ( $routeConfig as $routePattern => $routeParams ) {
-			$route = new Route( $routePattern, $routeParams );
-			$routes->add( $routePattern, $route );
-		}
-		return $routes;
-	}
+    throw new EndpointException(
+      "No transcoder found for any acceptable mime types",
+      EndpointException::CAN_NOT_FULFIL_ACCEPT_HEADER
+    );
+  }
 
-	protected function matchRoute( $request ) {
-		$context = new RequestContext();
-		$matcher = new UrlMatcher($this->routes, $context);
-		return $matcher->matchRequest( $request );
-	}
+  protected function initialiseRoutes( $routeConfig ) {
+    $routes = new RouteCollection();
+    foreach ( $routeConfig as $routePattern => $routeParams ) {
+      $route = new Route( $routePattern, $routeParams );
+      $routes->add( $routePattern, $route );
+    }
+    return $routes;
+  }
+
+  protected function matchRoute( $request ) {
+    $context = new RequestContext();
+    $matcher = new UrlMatcher($this->routes, $context);
+    return $matcher->matchRequest( $request );
+  }
 
 }
 
 class EndpointException extends \Exception {
-	const CAN_NOT_FULFIL_ACCEPT_HEADER = 1;
-	const UNSUPPORTED_METHOD = 2;
+  const CAN_NOT_FULFIL_ACCEPT_HEADER = 1;
+  const UNSUPPORTED_METHOD = 2;
 }
 
