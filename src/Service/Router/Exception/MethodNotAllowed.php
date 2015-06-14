af9d8365ecf076cfd6b6ce102b9be5f56d499960
Daniel Kendell <daniel@kendell.org>
2015-06-14 01:37:21 +0100

    should have added those sooner

---
+++ src/Service/Router/Exception/MethodNotAllowed.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Mduk\Service\Router\Exception;
+
+use Mduk\Service\Router\Exception;
+
+class MethodNotAllowed extends Exception {}
---
+++ src/Service/Router/Exception/NotFound.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Mduk\Service\Router\Exception;
+
+use Mduk\Service\Router\Exception;
+
+class NotFound extends Exception {}
