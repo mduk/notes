a1a28f66ce45d74860b2e21054027513fd0b7a4c
Daniel Kendell <daniel@kendell.org>
2015-06-14 02:57:14 +0100

    moved db to file

---
+++ db.sql
@@ -0,0 +1,30 @@
+CREATE TABLE user (
+  user_id INTEGER PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  role TEXT NOT NULL
+);
+
+INSERT INTO user VALUES ( 1, 'Daniel', 'daniel.kendell@gmail.com', 'admin' );
+INSERT INTO user VALUES ( 2, 'Slartibartfast', 'slartibartfast@magrathea.hg', 'user' );
+INSERT INTO user VALUES ( 3, 'Arthur Dent', 'arthur_dent@earth.hg', 'user' );
+INSERT INTO user VALUES ( 4, 'Ford Prefect', 'fprefect@megadodo-publications.hg', 'user' );
+
+CREATE TABLE note (
+  note_id INT PRIMARY KEY NOT NULL,
+  user_id INT NOT NULL,
+  body TEXT NOT NULL
+);
+
+INSERT INTO note VALUES ( 1, 1, 'note one' );
+INSERT INTO note VALUES ( 2, 1, 'note two' );
+INSERT INTO note VALUES ( 3, 1, 'note three' );
+INSERT INTO note VALUES ( 4, 1, 'note four' );
+INSERT INTO note VALUES ( 5, 1, 'note five' );
+INSERT INTO note VALUES ( 6, 1, 'note six' );
+INSERT INTO note VALUES ( 7, 1, 'note seven' );
+INSERT INTO note VALUES ( 8, 1, 'note eight' );
+INSERT INTO note VALUES ( 9, 1, 'note nine' );
+INSERT INTO note VALUES ( 10, 1, 'note ten' );
+INSERT INTO note VALUES ( 11, 1, 'note eleven' );
+INSERT INTO note VALUES ( 12, 1, 'note twelve' );
