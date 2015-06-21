08fbd7757263433f4f61f87218bba876370ac078
Daniel Kendell <daniel@kendell.org>
2015-06-21 19:12:32 +0100

    need to resolve file paths properly...

--- src/Stage/InitDb.php
+++ src/Stage/InitDb.php
@@ -12,7 +12,7 @@ use Mduk\Gowi\Http\Response;
 class InitDb implements Stage {
 
   public function execute( Application $app, Request $req, Response $res ) {
-    $dbPath = dirname( __FILE__ ) . '/../db.sq3';
+    $dbPath = dirname( __FILE__ ) . '/../../db.sq3';
     $initDb = !file_exists( $dbPath );
     $pdo = new PDO( 'sqlite:' . $dbPath ); 
     $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
