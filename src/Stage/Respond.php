e4b384b7e432ab299a522c1e84609b5209f1672b
Daniel Kendell <daniel@kendell.org>
2015-06-26 01:27:15 +0100

    if there's no response body, then there's no need to set a content type header, just ensure the content is empty

--- src/Stage/Respond.php
+++ src/Stage/Respond.php
@@ -10,8 +10,19 @@ use Mduk\Gowi\Http\Response;
 class Respond implements Stage {
 
   public function execute( Application $app, Request $req, Response $res ) {
-    $res->headers->set( 'Content-Type', $app->getConfig( 'http.response.content_type' ) );
-    $res->setContent( $app->getConfig( 'http.response.body' ) );
+    try {
+      $res->setContent( $app->getConfig( 'http.response.body' ) );
+      $res->headers->set( 'Content-Type', $app->getConfig( 'http.response.content_type' ) );
+    }
+    catch ( Application\Exception $e ) {
+      if ( $e->getCode() == Application\Exception::INVALID_CONFIG_KEY ) {
+        $res->setContent( '' );
+      }
+      else {
+        throw $e;
+      }
+    }
+
     return $res;
   }
 
