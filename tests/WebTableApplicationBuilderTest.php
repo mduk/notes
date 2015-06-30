715591d9d209365e75115ba1f3dcda9c826b6a32
Daniel Kendell <daniel@kendell.org>
2015-07-01 00:38:56 +0100

    added first test for web table application builder

---
+++ tests/WebTableApplicationBuilderTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Mduk;
+
+use Mduk\Gowi\Http\Request as HttpRequest;
+
+class WebTableApplicationBuilderTest extends \PHPUnit_Framework_TestCase {
+  protected $builder;
+
+  public function setUp() {
+    $this->builder = new WebTableApplicationBuilder;
+    $this->builder->setPdoConnection( 'sqlite:/Users/daniel/dev/notes/db.sq3' );
+    $this->builder->addTable( 'user', 'user_id', [ 'name', 'email', 'role' ] );
+  }
+  
+  public function testGetMany() {
+    $response = $this->builder->build()
+      ->run( HttpRequest::create( 'http://whatever/user' ) );
+
+    $this->assertEquals( 200, $response->getStatusCode(),
+      "Response code should have been 200" );
+
+    $this->assertEquals( 'application/json', $response->headers->get( 'Content-Type' ),
+      "Content-Type should have been application/json" );
+
+    $json = json_decode( $response->getContent() );
+
+    $this->assertObjectHasAttribute( 'objects', $json,
+      "Json should have contained an objects key" );
+
+    $this->assertGreaterThan( 2, count( $json->objects ),
+      "Json objects array should contain more than two items" );
+  }
+}
