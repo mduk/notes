395cf95807a2dd4a3e76c616a31a965958562a69
Daniel Kendell <daniel@kendell.org>
2015-07-01 00:43:38 +0100

    added a test for get one

--- tests/WebTableApplicationBuilderTest.php
+++ tests/WebTableApplicationBuilderTest.php
@@ -31,4 +31,26 @@ class WebTableApplicationBuilderTest extends \PHPUnit_Framework_TestCase {
     $this->assertGreaterThan( 2, count( $json->objects ),
       "Json objects array should contain more than two items" );
   }
+
+  public function testGetOne() {
+    $response = $this->builder->build()
+      ->run( HttpRequest::create( 'http://whatever/user/1' ) );
+
+    $this->assertEquals( 200, $response->getStatusCode(),
+      "Response code should have been 200" );
+
+    $this->assertEquals( 'application/json', $response->headers->get( 'Content-Type' ),
+      "Content-Type should have been application/json" );
+
+    $json = json_decode( $response->getContent() );
+
+    $this->assertObjectHasAttribute( 'name', $json,
+      "Json should have contained an name key" );
+
+    $this->assertObjectHasAttribute( 'email', $json,
+      "Json should have contained an email key" );
+
+    $this->assertObjectHasAttribute( 'role', $json,
+      "Json should have contained an role key" );
+  }
 }
